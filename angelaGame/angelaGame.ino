42e83f403f31140022cc47228a46ff860b13d250
Claudio Franzi <claudio.franzi@barsanti.edu.it>
2019-01-11 09:15:20 +0100

    11/01/19 - LED rosso

--- angelaGame/angelaGame.ino
+++ angelaGame/angelaGame.ino
@@ -1,9 +1,20 @@
 void setup() {
   // put your setup code here, to run once:
 
+
+  int LEDrosso = 8;
+  int LEDblu = 9;
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+
+  if (digitalRead(LEDrosso) == HIGH)
+  {
+    digitalWrite(LEDrosso) = LOW;
+  }
+
+  
 }
